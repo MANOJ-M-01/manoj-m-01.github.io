2801b2eb58870be6211497de1bef568852550d0a
manoj-m-01 <manoj904378@gmail.com>
2023-12-03 22:44:57 +0530

    banner added

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Banner() {
+    return (
+        <div className='container'>
+            <div className="backdrop-content">
+                <h1> Hey there! &nbsp; I'm Manoj. </h1>
+            </div>
+        </div>
+    )
+}
+export default Banner
