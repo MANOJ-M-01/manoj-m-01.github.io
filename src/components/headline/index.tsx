72b2655ba61bc09e4dcb9134913527e1f191a3a9
manoj-m-01 <manoj904378@gmail.com>
2023-12-03 22:45:25 +0530

    headline page added

---
+++ src/components/headline/index.tsx
@@ -0,0 +1,29 @@
+import './style.css'
+
+function Headline() {
+  return (
+    <div className='grid'>
+      <div></div>
+      <div className='sdsd'>
+        <h3>Short Bio</h3>
+        <p>
+          With an emphasis on producing high-quality software products, I have over three years of experience as a full-stack developer and a skill set that encompasses planning, designing, developing, and testing.
+          I'm a versatile and dynamic developer because I'm skilled in both front-end and back-end development.
+
+          I've worked on a wide variety of projects across my career, such as static websites, e-commerce websites, and billing software.
+
+          I'm working on a really cool project right now that pushes my abilities and requires me to think creatively.
+
+          I have a strong interest in investigating the newest technological developments and using creative problem-solving to produce original solutions.
+
+          I'm constantly looking for fresh challenges and chances to develop.
+
+          Together, let's work together to create something genuinely amazing!
+        </p>
+        <button>Let's build something</button>
+      </div>
+    </div>
+
+  )
+}
+export default Headline
