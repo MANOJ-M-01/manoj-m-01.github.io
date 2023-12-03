6cb501fce2a30c10abeb999ba96e904d412d4327
manoj-m-01 <manoj904378@gmail.com>
2023-12-03 22:45:35 +0530

    footer added

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-bottom">
+        <div className="copyright">
+          <span>© Manoj Portfolio 2023. All rights reserved.</span>Built with
+          <a href="https://www.manoj-m-01.github.io/" target="_blank"><h3>💓</h3></a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+export default Footer
