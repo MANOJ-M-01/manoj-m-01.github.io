0da7ec16d5169a18ce98839ee8c1a06a9f3f61f9
manoj-m-01 <manoj904378@gmail.com>
2023-12-10 18:34:34 +0530

    link updated

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,7 @@ function Footer() {
       <div className="footer-bottom">
         <div className="copyright">
           <span>© Manoj Portfolio 2023. All rights reserved.</span>Built with
-          <a href="https://www.manoj-m-01.github.io/" target="_blank"><h3>💓</h3></a>
+          <a href="https://manoj-m-01.github.io/" target="_blank"><h3>💓</h3></a>
         </div>
       </div>
     </footer>
